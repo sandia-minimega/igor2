9420cec8723586baa3e9a21c205d40dcef25464e
Allen Bagwell <afbagwe@sandia.gov>
2024-08-12 15:26:38 -0700

    [#41] Hosts can be blocked even if reserved.

--- internal/app/igor-server/host_block.go
+++ internal/app/igor-server/host_block.go
@@ -6,8 +6,10 @@ package igorserver
 
 import (
 	"fmt"
+	"igor2/internal/pkg/common"
 	"net/http"
 	"sort"
+	"time"
 
 	"gorm.io/gorm"
 )
@@ -31,7 +33,7 @@ func checkBlockParams(powerParams map[string]interface{}) (bool, []string, int, 
 }
 
 // Runs the actual power command for the service that controls host power options.
-func doUpdateBlockHosts(blockAction bool, hostList []string) (status int, err error) {
+func doUpdateBlockHosts(blockAction bool, hostList []string, r *http.Request) (status int, err error) {
 
 	status = http.StatusInternalServerError // default status, overridden at end if no errors
 
@@ -46,29 +48,91 @@ func doUpdateBlockHosts(blockAction bool, hostList []string) (status int, err er
 			return fmt.Errorf("no hosts found for given host list: %v", hostList)
 		}
 
-		var hostChangeState HostState
 		if blockAction {
-			hostChangeState = HostBlocked
-		} else {
-			hostChangeState = HostAvailable
-		}
 
-		for _, h := range hList {
-			if h.State == HostReserved || len(h.Reservations) > 0 {
-				// you can't change the state to blocked on an active reservation
-				if hostChangeState == HostBlocked {
-					status = http.StatusConflict
-					return fmt.Errorf("cannot block a host with an active reservation")
-				}
-				// you can't change the state to available if the host isn't currently blocked or available
-				if hostChangeState == HostAvailable && h.State != HostBlocked && h.State != HostAvailable {
-					status = http.StatusConflict
-					return fmt.Errorf("cannot un-block a non-blocked host")
+			blockedRes := make(map[string]Reservation)
+			for _, h := range hList {
+				if h.State == HostReserved {
+					for _, res := range h.Reservations {
+						if res.IsActive(time.Now()) {
+							blockedRes[res.Name] = res
+						}
+					}
 				}
 			}
-		}
 
-		return dbEditHosts(hList, map[string]interface{}{"State": hostChangeState}, tx) // uses default err status
+			blockErr := dbEditHosts(hList, map[string]interface{}{"State": HostBlocked}, tx)
+			if blockErr != nil {
+				return blockErr
+			}
+
+			if len(blockedRes) > 0 {
+				actionUser := getUserFromContext(r)
+				isElevated := userElevated(actionUser.Name)
+
+				for _, bRes := range blockedRes {
+					var blockList []string
+					var clusterName = ""
+					for _, host := range hList {
+						for _, hostRes := range host.Reservations {
+							if bRes.Name == hostRes.Name {
+								blockList = append(blockList, host.HostName)
+								clusterName = host.Cluster.Name
+							}
+						}
+					}
+
+					res, _, _ := getReservations([]string{bRes.Name}, tx)
+
+					blockEvent := makeResEditNotifyEvent(EmailResBlock, &res[0], clusterName, actionUser, isElevated, common.UnsplitList(blockList))
+					if blockEvent != nil {
+						resNotifyChan <- *blockEvent
+					}
+				}
+			}
+
+			return nil
+		} else {
+
+			for _, h := range hList {
+				if h.State != HostBlocked {
+					status = http.StatusConflict
+					return fmt.Errorf("cannot un-block a non-blocked host: '%s'", h.HostName)
+				}
+			}
+
+			var hAvailList []Host
+			var hReservedList []Host
+
+			for _, h := range hList {
+				if len(h.Reservations) == 0 {
+					hAvailList = append(hAvailList, h) // no reservations, set to available
+				} else {
+					for _, r := range h.Reservations {
+						if r.IsActive(time.Now()) {
+							hReservedList = append(hReservedList, h) // current reservation, set to reserved
+						} else {
+							hAvailList = append(hAvailList, h) // has a future reservation, set to available
+						}
+					}
+				}
+			}
+
+			var unblockErr error
+
+			// do unreserved first
+			if len(hAvailList) > 0 {
+				unblockErr = dbEditHosts(hAvailList, map[string]interface{}{"State": HostAvailable}, tx)
+				if unblockErr != nil {
+					return unblockErr
+				}
+			}
+
+			if len(hReservedList) > 0 {
+				unblockErr = dbEditHosts(hReservedList, map[string]interface{}{"State": HostReserved}, tx)
+			}
+			return unblockErr
+		}
 
 	}); err == nil {
 		status = http.StatusOK
