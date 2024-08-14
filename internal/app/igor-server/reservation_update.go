3d693a0e1ae622134c9fc763bd4386668646d8a0
Allen Bagwell <afbagwe@sandia.gov>
2024-08-14 11:40:01 -0700

    [#41] Deny extending a reservation with blocked hosts unless user is elevated.

--- internal/app/igor-server/reservation_update.go
+++ internal/app/igor-server/reservation_update.go
@@ -220,11 +220,20 @@ func parseDrop(res *Reservation, dropList string, tx *gorm.DB) (map[string]inter
 	return changes, http.StatusOK, nil
 }
 
-// parseExtend checks that the extend parameter has correct syntax and the modified end time
+// parseExtend checks that the 'extend' parameter has correct syntax and the modified end time
 // it creates doesn't collide with existing reservations and/or host policies.
 func parseExtend(res *Reservation, extendTime string, isActionUserElevated bool, r *http.Request, tx *gorm.DB) (map[string]interface{}, int, error) {
 
 	clog := hlog.FromRequest(r)
+
+	if !isActionUserElevated {
+		for _, h := range res.Hosts {
+			if h.State == HostBlocked {
+				return nil, http.StatusConflict,
+					fmt.Errorf("unable to extend - one or more nodes in this reservation has a blocked status. Contact your cluster admin team.")
+			}
+		}
+	}
 
 	hostNameList := namesOfHosts(res.Hosts)
 
